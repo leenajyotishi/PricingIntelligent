eb973f0b149187eeb9761a6d478934cc24060bd8
leenajyotishi <69509483+leenajyotishi@users.noreply.github.com>
2020-10-29 07:00:09 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-# -*- coding: utf-8 -*-
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 
-from flask import Flask,render_template,request
-import  pandas as pd
-from fbprophet import Prophet
-app=Flask(__name__)
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
 
-
-
-@app.route('/',methods=['GET','POST'])
-def Index():
+@app.route('/')
+def home():
     return render_template('index.html')
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
-    
-@app.route('/data',methods=['GET','POST'])
-def data():
-    if request.method=='POST':
-        
-      return render_template('index.html')
-        
-if __name__=='__main__':
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
