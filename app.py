d4cf6209f0fa1e410f3fe064a0851e905f012e5e
leenajyotishi <69509483+leenajyotishi@users.noreply.github.com>
2020-10-29 06:44:40 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -17,35 +17,7 @@ def Index():
 def data():
     if request.method=='POST':
         
-       # file=request.form['upload-file']
-        profile = request.files['upload-file']
-        Noofmonths=int(request.form['Noofmonths'])
-        profile.save(profile.filename)  
-        data=pd.read_excel(profile.filename)
-        df2 = pd.DataFrame(data)
-        # df2.set_index('Product', inplace=True)
-       # data1 = {'Product':['P1', 'P2'],'2017-01-01':['12','92'],'2017-02-01':['13','99'],'2017-03-01':['15','98'],
-       #'2017-04-01':['12','95']}
-        #df1 = pd.DataFrame(df2)
-        gapminder_tidy = df2.melt(id_vars=['Pro'], 
-                              var_name='year', 
-                              value_name='Amount')
-        df = gapminder_tidy.rename(columns={'year': 'ds', 'Amount':'y'})
-        grouped = df.groupby('Pro')
-        final = pd.DataFrame()
-        for g in grouped.groups:
-            group = grouped.get_group(g)
-            m = Prophet()
-            m.fit(group)
-            future = m.make_future_dataframe(periods=Noofmonths, freq='M')
-            forecast = m.predict(future)    
-            forecast = forecast.rename(columns={'yhat': g})
-            final = pd.merge(final, forecast.set_index('ds'), how='outer', left_index=True, right_index=True)
-            
-        final = final[[ g for g in grouped.groups.keys()]]
-           
-            
-        return render_template('data.html',data=final.to_html())
+      return render_template('index.html')
         
 if __name__=='__main__':
     app.run(debug=True)
