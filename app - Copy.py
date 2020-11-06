b7eadc29640d780c8f3dec063f933936d09c531c
leenajyotishi <69509483+leenajyotishi@users.noreply.github.com>
2020-11-06 05:42:06 -0500

    Add files via upload

--- app - Copy.py
+++ app - Copy.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, jsonify, render_template,redirect,url_for
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
 from fbprophet import Prophet
-import os,json,boto3
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('Demo.html')
+    return render_template('index.html')
     
 @app.route('/data',methods=['POST'])        
 def data():
@@ -33,30 +32,6 @@ def data():
         final = pd.merge(final, forecast.set_index('ds'), how='outer', left_index=True, right_index=True)
     final = final[[ g for g in grouped.groups.keys()]]
     return render_template('data.html', data=final.to_html())
- 
-@app.route('/sign_s3/')
-def sign_s3():
-  S3_BUCKET = os.environ.get('S3_BUCKET')
-
-  file_name = request.args.get('file_name')
-  file_type = request.args.get('file_type')
-
-  s3 = boto3.client('s3')
-
-  presigned_post = s3.generate_presigned_post(
-    Bucket = S3_BUCKET,
-    Key = file_name,
-    Fields = {"acl": "public-read", "Content-Type": file_type},
-    Conditions = [
-      {"acl": "public-read"},
-      {"Content-Type": file_type}
-    ],
-    ExpiresIn = 3600
-  )
-
-  return json.dumps({
-    'data': presigned_post,
-    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
-  })       
+        
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0',port=8080)
+    app.run(debug=True)
